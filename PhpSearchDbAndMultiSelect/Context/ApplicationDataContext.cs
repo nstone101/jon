5c0fccf355e3f2fba7939243d0e6d596a9c2ef6a
Nick <nstone101@gmail.com>
2019-12-25 08:37:55 -0600

    Add project files.

---
+++ PhpSearchDbAndMultiSelect/Context/ApplicationDataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PhpSearchDbAndMultiSelect.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace PhpSearchDbAndMultiSelect.Context
+{
+    public class ApplicationDataContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationDataContext()
+            : base("DefaultConnection")
+        { }
+
+        public System.Data.Entity.DbSet<AppUser> AppUsers { get; set; }
+    }
+}
---
+++ PhpSearchDbAndMultiSelect/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhpSearchDbAndMultiSelect.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title="Nick stone Development";
+return View();
+        }
+    }
+}
